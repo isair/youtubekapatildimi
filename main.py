29994bcbe5a57d42217ec098e634795f60f4ee1f
Baris Sencan <run32a50mch@gmail.com>
2014-01-14 15:04:33 +0200

    Basic home page design.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 main.py
 Jan 14, 2014
@@ -7,14 +9,19 @@ Copyright (C) 2014 Barış Şencan
 import os
 import redis
 import dns.resolver
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+# Static file loading helper.
+app.jinja_env.globals['static'] = (
+    lambda filename: url_for('static', filename=filename))
+
+# Redis configuration.
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
 
 
 @app.route('/')
 def home():
-    return 'HAYIR'
+    return render_template('home.html')
