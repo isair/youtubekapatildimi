6694314a264197bead5a508d05b4ef847c843e02
Baris Sencan <run32a50mch@gmail.com>
2014-01-14 14:06:40 +0200

    Using redis for data storage.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
+import redis
+import dns.resolver
 from flask import Flask
 
 app = Flask(__name__)
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis = redis.from_url(redis_url)
 
 
 @app.route('/')
